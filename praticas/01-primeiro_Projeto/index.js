3b7d92a96a33692def748af4d9cd6c88f64f03df
Wendels1 <ferreirawendel774@gmail.com>
2025-08-20 21:29:44 -0300

    Criando o 1 projeto Node

---
+++ praticas/01-primeiro_Projeto/index.js
@@ -0,0 +1,20 @@
+console.log("Criando meu primeiro arquivo em Node")
+
+let nome = "Wendel"
+
+let sebrenome = "Ferreira"
+
+console.log("Meu nome é " + nome + " " + sebrenome)
+
+let idade = 21
+console.log("A idade do "+ nome +  " "+ sebrenome + " é " + idade + " anos")
+
+let exercicios = 0.5
+
+let trabalho =  4.5
+
+let prova = 3
+
+let NotaResultado = (exercicios + trabalho + prova)
+
+console.log("A nota do aluno " + nome + " " + sebrenome + " é " + NotaResultado)
