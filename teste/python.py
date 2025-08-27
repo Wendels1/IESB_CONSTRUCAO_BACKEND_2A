149acfc8c8ecd9a647c47405efb928eaaaddd061
Wendels1 <ferreirawendel774@gmail.com>
2025-08-27 20:16:33 -0300

    Teste de outra matéria em python deletado

--- teste/python.py
+++
@@ -1,8 +0,0 @@
-import sys
-import time
-
-i = 0
-while i <= 10:
-    sys.stdout.write("\x0DProgresso:  "+str (i) +"/10")
-    time.sleep(0.5)
-    i += 1
--- teste/teste.py
+++
@@ -1,4 +0,0 @@
-import sys
-
-file = open("sum.txt", "r")
-
