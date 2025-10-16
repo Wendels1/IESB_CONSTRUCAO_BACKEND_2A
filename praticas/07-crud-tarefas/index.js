f53f7e169ee5aa7154fd268110b302e01b22a423
Wendels1 <ferreirawendel774@gmail.com>
2025-10-15 21:32:00 -0300

    Aula 07 crud de tarefas

--- praticas/07-crud-tarefas/index.js
+++ praticas/07-crud-tarefas/index.js
@@ -20,8 +20,57 @@ mongoose.connect(url)
     console.log("Conectado ao MongoDB")
   })
   .catch(err => {
-    console.log("Erro ao conectar no banco Mo'ngoDB: ", err)
+    console.log("Erro ao conectar no banco MongoDB: ", err)
   })
+
+// interface com o banco
+const tarefaModel = mongoose.model('Tarefas', new mongoose.Schema(
+  {
+    nome: String,
+    
+  }
+))
+
+//CRUD
+
+//Create
+app.post('/tarefas', async(req, res, next) => {
+  const tarefa = req.body
+  if(!tarefa.nome){
+    return res.status(400).send({error: "O campo nome é obrigatório"})
+  }
+ const tarefaCriada = await tarefaModel.create(tarefa)
+  res.status(201).send(tarefaCriada)    
+})
+
+//Read
+app.get('/tarefas', async(req, res, next) => {
+  const tarefas = await tarefaModel.find()
+  res.json(tarefas)
+})
+
+//UPDATE
+app.put('/tarefas/:id', async(req, res, next) => {
+  const id = req.params.id
+  const tarefa = req.body
+  if(!tarefa.nome){
+    return res.status(400).send({error: "O campo nome é obrigatório"})
+  }
+  const tarefaAtualizada = await tarefaModel.findByIdAndUpdate(id, tarefa, {new: true})
+  if(!tarefaAtualizada){
+    return res.status(404).send({error: "Tarefa não encontrada"})
+  }
+  res.json(tarefaAtualizada)
+})
+
+//DELETE
+app.delete('/tarefas/:id', async(req, res, next) => {
+  const id = req.params.id
+  await tarefaModel.findByIdAndDelete(id)
+  res.json({
+    message: "Tarefa excluída com sucesso"
+  })
+})
 
 app.listen(3000, () => {
   console.log("Aplicação rodando em http://localhost:3000")
